c7d6dd76cca48dd49d8df7ca84b853f16a8a77ea
sayden <mariocaster@gmail.com>
2016-06-12 23:56:51 +0200

    Bubble merge and shell

--- sort/bubble.go
+++ sort/bubble.go
@@ -1,1 +1,93 @@
 package sort
+
+func BubbleUint8(u []uint8) []uint8 {
+	newList := make([]uint8, len(u))
+	copy(newList, u)
+
+	for {
+		hasSwap := false
+		for i := 0; i < len(newList)-1; i++ {
+			if newList[i] > newList[i+1] {
+				old := newList[i]
+				newList[i] = newList[i+1]
+				newList[i+1] = old
+				hasSwap = true
+			}
+		}
+
+		if !hasSwap {
+			break
+		}
+	}
+
+	return newList
+}
+
+func BubbleUint16(u []uint16) []uint16 {
+	newList := make([]uint16, len(u))
+	copy(newList, u)
+
+	for {
+		hasSwap := false
+		for i := 0; i < len(newList)-1; i++ {
+			if newList[i] > newList[i+1] {
+				old := newList[i]
+				newList[i] = newList[i+1]
+				newList[i+1] = old
+				hasSwap = true
+			}
+		}
+
+		if !hasSwap {
+			break
+		}
+	}
+
+	return newList
+}
+
+func BubbleUint32(u []uint32) []uint32 {
+	newList := make([]uint32, len(u))
+	copy(newList, u)
+
+	for {
+		hasSwap := false
+		for i := 0; i < len(newList)-1; i++ {
+			if newList[i] > newList[i+1] {
+				old := newList[i]
+				newList[i] = newList[i+1]
+				newList[i+1] = old
+				hasSwap = true
+			}
+		}
+
+		if !hasSwap {
+			break
+		}
+	}
+
+	return newList
+}
+
+func BubbleUint64(u []uint64) []uint64 {
+	newList := make([]uint64, len(u))
+	copy(newList, u)
+
+	for {
+		hasSwap := false
+		for i := 0; i < len(newList)-1; i++ {
+			if newList[i] > newList[i+1] {
+				old := newList[i]
+				newList[i] = newList[i+1]
+				newList[i+1] = old
+				hasSwap = true
+			}
+		}
+
+		if !hasSwap {
+			break
+		}
+	}
+
+	return newList
+}
--- sort/shell.go
+++ sort/shell.go
@@ -1,1 +1,57 @@
 package sort
+
+func ShellUint8(u []uint8, j int) (newL []uint8) {
+	if j == 0 {
+		j = 4
+	}
+
+	newL = make([]uint8, len(u))
+	copy(newL, u)
+
+	//Interval of 4
+	interval := j << 1
+
+	for interval>>1 > 0 {
+		interval = interval >> 1
+
+		for i := 0; i < len(newL)-interval; i++ {
+			left := newL[i]
+			right := newL[i+interval]
+
+			if right < left {
+				newL[i] = right
+				newL[i+interval] = left
+			}
+		}
+	}
+
+	return
+}
+
+func ShellInt(u []int, jump int) (newL []int) {
+	if jump == 0 {
+		jump = 4
+	}
+
+	newL = make([]int, len(u))
+	copy(newL, u)
+
+	//Interval of 4
+	interval := jump << 1
+
+	for interval>>1 > 0 {
+		interval = interval >> 1
+
+		for i := 0; i < len(newL)-interval; i++ {
+			left := newL[i]
+			right := newL[i+interval]
+
+			if right < left {
+				newL[i] = right
+				newL[i+interval] = left
+			}
+		}
+	}
+
+	return
+}
